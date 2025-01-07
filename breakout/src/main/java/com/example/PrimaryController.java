ba059eb0e217e0e32f5f8a1bf607c789efef6367
slayervictor <victor.r2002@gmail.com>
2025-01-07 11:21:09 +0100

    smol changes

--- breakout/src/main/java/com/example/PrimaryController.java
+++ breakout/src/main/java/com/example/PrimaryController.java
@@ -11,17 +11,8 @@ import GameBoard.*;
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() {
-        
-    // Create a new scene programmatically
-        StackPane root = new StackPane();
-        root.getChildren().add(new Label("Breakout"));
-        
-        Scene newScene = View.createGameboardView(); // Set the desired width and height
-
-        // Get the current stage and set the new scene
-        Stage currentStage = (Stage) root.getScene().getWindow();
-        currentStage.setScene(newScene);
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
     }
 
 }
