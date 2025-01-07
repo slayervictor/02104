31a57608d528c87a17890dee4f037363f847c8d7
Raaaainbow <seba7204@gmail.com>
2025-01-07 14:17:31 +0100

    Bounce and change pos added

--- breakout/src/main/java/GameBoard/Model.java
+++ breakout/src/main/java/GameBoard/Model.java
@@ -57,9 +57,34 @@ class Ball {
         velo[1] = dY;
     }
 
+    public int[] getVelo() {
+        return velo;
+    }
+
     public void setPos(int x, int y) {
         pos[0] = x;
         pos[1] = y;
     }
-}
 
+    public int[] getPos() {
+        return pos;
+    }
+
+    // probably a faster way of doing this
+    public int[] nextPos(int[] pos, int[] velo) {
+        int[] temp = {0,0};
+        for (int i = 0; i <= 1; i++) {
+            temp[i] = pos[i] + velo[i];
+        }
+        return temp;
+    }
+
+    public void wallBounce(int[] velo) {
+        velo[0] *= -1;
+    }
+
+    public void roofBounce(int[] velo) {
+        velo[1] *= -1;
+    }
+
+}
