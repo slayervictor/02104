021030174426fbbfe83e3efcb2e60a6fc3160251
Raaaainbow <seba7204@gmail.com>
2025-01-08 22:23:36 +0100

    contructor added

--- breakout/src/main/java/GameBoard/Ball.java
+++ breakout/src/main/java/GameBoard/Ball.java
@@ -1,31 +1,51 @@
 package GameBoard;
 
+import com.example.App;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
 // Change int to double
 public class Ball {
-    private int[] velo = new int[]{0,0};    //change inital velocity when program works
-    private int[] pos = new int[]{0,0};
+    private double[] velo = new double[]{0,0};    //change inital velocity when program works
+    private double[] pos = new double[]{0,0};
+    private double speed = 1;   // change value to increase or decrease ball speed
+    private Rectangle rect;
     
-    public void setVelo(int dX, int dY) {
+    public Ball(double x, double y) {
+        double dX = (Math.random()*2)-1;  // -1 < rand < 1         
+        double dY = Math.sqrt(1-Math.pow(dX, 2));      // Ensures hypotenuse is always 1 for any x
+        setPos(x, y);
+        setVelo(dX, dY);
+        rect = new Rectangle(x,y,13,10);
+        rect.setFill(Color.rgb(158, 158, 158));
+        App.addElement(rect);
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setVelo(double dX, double dY) {
         velo[0] = dX;
         velo[1] = dY;
     }
 
-    public int[] getVelo() {
+    public double[] getVelo() {
         return velo;
     }
 
-    public void setPos(int x, int y) {
+    public void setPos(double x, double y) {
         pos[0] = x;
         pos[1] = y;
     }
 
-    public int[] getPos() {
+    public double[] getPos() {
         return pos;
     }
 
     // probably a faster way of doing this
-    public int[] nextPos(int[] pos, int[] velo) {
-        int[] temp = {0,0};
+    public double[] nextPos(double[] pos, double[] velo) {
+        double[] temp = {0,0};
         for (int i = 0; i <= 1; i++) {
             temp[i] = pos[i] + velo[i];
         }
@@ -39,6 +59,4 @@ public class Ball {
     public void roofBounce(int[] velo) {
         velo[1] *= -1;
     }
-
-
 }
